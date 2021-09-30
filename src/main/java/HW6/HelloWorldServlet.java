12642accfa3e846aad04fc948de99d7f47b33d7a
Maverickspb78 <Maverickspb78@gmail.com>
2021-09-30 13:45:28 +0300

    HW6

---
+++ src/main/java/HW6/HelloWorldServlet.java
@@ -0,0 +1,24 @@
+package HW6;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/hello")
+public class HelloWorldServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+
+        PrintWriter printOut = resp.getWriter();
+        printOut.write("Hello World");
+
+
+    }
+
+}
